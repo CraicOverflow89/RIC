f6484f728467d8d3de9aac0bd13ff5315c7206c8
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-29 14:29:58 +0000

    Updated states to go to main.

--- ric/states/intro.py
+++ ric/states/intro.py
@@ -2,15 +2,16 @@ from riem.core import Application, State
 from riem.graphics import Align, Graphics, ImageLoader
 from riem.library import Point
 
-class StateMain(State):
+class StateIntroRIC(State):
 
 	def __init__(self, app: Application) -> None:
 		super().__init__(app)
+		self.add_event(2000, lambda: self.app.state_update("StateMain"))
 
 	def render(self, gfx: Graphics) -> None:
 
-		# Render Test
-		gfx.draw_text("Hello Word", Point(0, 0))
+		# Render Logo
+		gfx.draw_image(ImageLoader.load("logo"), Point(self.app.get_dimensions().width / 2, self.app.get_dimensions().height / 2), Align.CENTER)
 
 	def tick(self) -> None:
 		pass
