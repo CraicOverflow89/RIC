6bc38ee73facf82b69310691288503d86f8b1d6a
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-29 14:26:59 +0000

    Created the StateIntroRIC state.

--- ric/app.py
+++ ric/app.py
@@ -3,6 +3,6 @@ from riem.core import Application
 class EditorApp(Application):
 
 	def __init__(self):
-		super().__init__("RIC Editor", "Main", "ric/states")
+		super().__init__("RIC Editor", "StateIntroRIC", "ric/states", debug = "+")
 
 EditorApp()
--- ric/states/main.py
+++ ric/states/main.py
@@ -1,4 +1,16 @@
-from riem.core import State
+from riem.core import Application, State
+from riem.graphics import Align, Graphics, ImageLoader
+from riem.library import Point
 
-class StateMain(State):
-	pass
+class StateIntroRIC(State):
+
+	def __init__(self, app: Application) -> None:
+		super().__init__(app)
+
+	def render(self, gfx: Graphics) -> None:
+
+		# Render Logo
+		gfx.draw_image(ImageLoader.load("logo"), Point(self.app.get_dimensions().width / 2, self.app.get_dimensions().height / 2), Align.CENTER)
+
+	def tick(self) -> None:
+		pass
