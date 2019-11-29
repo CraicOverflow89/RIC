a8616ebd72549c39cfbe41a4beaf96ee29731000
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-29 14:42:56 +0000

    Making use of the screeninfo module to determine main monitor width and height.

--- ric/app.py
+++ ric/app.py
@@ -1,8 +1,11 @@
 from riem.core import Application
+from riem.library import Dimensions
+from screeninfo import get_monitors
 
 class EditorApp(Application):
 
 	def __init__(self):
-		super().__init__("RIC Editor", "StateIntroRIC", "ric/states", debug = "+")
+		monitor = get_monitors()[0]
+		super().__init__("RIC Editor", "StateIntroRIC", "ric/states", {}, None, Dimensions(monitor.width, monitor.height), 250, debug = "+")
 
 EditorApp()
