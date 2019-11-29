1683e2fa47f3d2c14ffa15b9b497d725d5d0d1ac
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-29 15:13:50 +0000

    Making use of fullscreen attribute.

--- ric/app.py
+++ ric/app.py
@@ -6,6 +6,6 @@ class EditorApp(Application):
 
 	def __init__(self):
 		monitor = get_monitors()[0]
-		super().__init__("RIC Editor", "StateIntroRIC", "ric/states", {}, None, Dimensions(monitor.width, monitor.height), 250, debug = "+")
+		super().__init__("RIC Editor", "StateIntroRIC", "ric/states", {}, None, Dimensions(monitor.width, monitor.height), 250, debug = "+", fullscreen = True)
 
 EditorApp()
